31e35647a786a55f9b8994320bbe424ef902e6aa
Ox2bc <45404918+Ox2bc@users.noreply.github.com>
2021-11-08 12:19:13 -0500

    11/8/21 12:19PM

--- xchange/src/components/NavBar.vue
+++ xchange/src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <section class="nav-bar">
     <img class="logo" src="../assets/media/x.png" alt="" />
     <router-link class="route" to="/">Current Info</router-link> |
-    <router-link class="route" to="/about">Historical Data</router-link>
+    <router-link class="route" to="/Historical">Historical Data</router-link>
   </section>
 </template>
 
